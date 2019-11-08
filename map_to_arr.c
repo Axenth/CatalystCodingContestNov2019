92e5552f34a9a582dee56c3488ebf255c92ff591
nikander100 <ndvdschee@gmail.com>
2019-11-08 20:16:09 +0100

    removed include

--- map_to_arr.c
+++ map_to_arr.c
@@ -6,13 +6,12 @@
 /*   By: nvan-der <nvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/08 15:28:56 by nvan-der       #+#    #+#                */
-/*   Updated: 2019/11/08 17:47:58 by jlensing      ########   odam.nl         */
+/*   Updated: 2019/11/08 17:47:58 by nvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "features.h"
-#include <stdio.h>
 
 char	**read_map(const char *buff)
 {
